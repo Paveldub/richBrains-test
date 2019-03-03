a418c3ef7261f390be29715db9db731b54539572
Mac <mac@MacBook-Pro-Mac.local>
2019-03-03 22:46:02 +0300

    corrections

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,3 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
-
 sal();
 
 const items = document.querySelectorAll('.tabs-menu__item');
